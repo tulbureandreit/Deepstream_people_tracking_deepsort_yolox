adef2a326a971319f0aab5e8d2ab0d9d35fed282
AndreiT <smandrei96@yahoo.de>
2022-03-06 12:00:36 +0200

    yolox deepstream version

---
+++ deepstream_YOLOx/nvdsinfer_custom_impl_yolox/nvdsparsebbox_yolox.cpp
@@ -0,0 +1,369 @@
+
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <cuda_runtime_api.h>
+
+//#include "gstnvdsmeta.h"
+#include <omp.h>
+#include <opencv2/opencv.hpp>
+#include "nvdsinfer_custom_impl.h"
+#define MAX_TRACKING_ID_LEN 16
+
+
+#define DEVICE 0  // GPU id
+#define NMS_THRESH 0.5
+#define BBOX_CONF_THRESH 0.5
+
+
+static const int INPUT_W = 640;
+static const int INPUT_H = 640;
+const char* INPUT_BLOB_NAME = "images";
+const char* OUTPUT_BLOB_NAME = "output";
+
+//guint vehicle_count = 0;
+
+struct Object
+{
+    cv::Rect_<float> rect;
+    int label;
+    float prob;
+};
+
+struct GridAndStride
+{
+    int grid0;
+    int grid1;
+    int stride;
+};
+
+static GstPadProbeReturn
+osd_sink_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info,
+    gpointer u_data)
+{
+    GstBuffer *buf = (GstBuffer *) info->data;
+    guint num_rects = 0;
+    NvDsObjectMeta *obj_meta = NULL;
+    guint vehicle_count = 0;
+    NvDsMetaList * l_frame = NULL;
+    NvDsMetaList * l_obj = NULL;
+    NvDsDisplayMeta *display_meta = NULL;
+
+    NvDsBatchMeta *batch_meta = gst_buffer_get_nvds_batch_meta (buf);
+
+    for (l_frame = batch_meta->frame_meta_list; l_frame != NULL; l_frame = l_frame->next) {
+        NvDsFrameMeta *frame_meta = (NvDsFrameMeta *) (l_frame->data);
+        int offset = 0;
+        
+
+        for (l_obj = frame_meta->obj_meta_list; l_obj != NULL;
+                l_obj = l_obj->next) {
+            obj_meta = (NvDsObjectMeta *) (l_obj->data);
+            if (obj_meta->class_id == PGIE_CLASS_ID_VEHICLE) {
+                vehicle_count++;
+                
+            }
+        }
+        display_meta = nvds_acquire_display_meta_from_pool(batch_meta);
+        NvOSD_TextParams *txt_params  = &display_meta->text_params[0];
+        display_meta->num_labels = 3;
+        txt_params->display_text = g_malloc0 (MAX_DISPLAY_LEN);
+        //offset = snprintf(txt_params->display_text, MAX_DISPLAY_LEN, "Person = %d ", person_count);
+        offset = snprintf(txt_params->display_text + offset , MAX_DISPLAY_LEN, "Vehicle = %d ", vehicle_count);
+        //offset2 = snprintf(txt_params->display_text + offset2 , MAX_DISPLAY_LEN, "Vehicle = %d ", vehicle_count);
+
+        /* Now set the offsets where the string should appear */
+        txt_params->x_offset = 10;
+        txt_params->y_offset = 12;
+
+        txt_params->x_offset2 = 10;
+        txt_params->y_offset2 = 12;
+
+        /* Font , font-color and font-size */
+        txt_params->font_params.font_name = "Serif";
+        txt_params->font_params.font_size = 10;
+        txt_params->font_params.font_color.red = 1.0;
+        txt_params->font_params.font_color.green = 1.0;
+        txt_params->font_params.font_color.blue = 1.0;
+        txt_params->font_params.font_color.alpha = 1.0;
+
+        /* Text background color */
+        txt_params->set_bg_clr = 1;
+        txt_params->text_bg_clr.red = 0.0;
+        txt_params->text_bg_clr.green = 0.0;
+        txt_params->text_bg_clr.blue = 0.0;
+        txt_params->text_bg_clr.alpha = 1.0;
+
+        nvds_add_display_meta_to_frame(frame_meta, display_meta);
+    }
+
+    g_print ("vehicle Count = %d \n", vehicle_count);
+
+    return GST_PAD_PROBE_OK;
+}
+
+
+static void generate_grids_and_stride(const int target_size, std::vector<int>& strides, std::vector<GridAndStride>& grid_strides)
+{
+    for (auto stride : strides)
+    {
+        int num_grid = target_size / stride;
+        for (int g1 = 0; g1 < num_grid; g1++)
+        {
+            for (int g0 = 0; g0 < num_grid; g0++)
+            {
+                grid_strides.push_back((GridAndStride){g0, g1, stride});
+            }
+        }
+    }
+}
+
+static inline float intersection_area(const Object& a, const Object& b)
+{
+    cv::Rect_<float> inter = a.rect & b.rect;
+    return inter.area();
+}
+
+static void qsort_descent_inplace(std::vector<Object>& faceobjects, int left, int right)
+{
+    int i = left;
+    int j = right;
+    float p = faceobjects[(left + right) / 2].prob;
+
+    while (i <= j)
+    {
+        while (faceobjects[i].prob > p)
+            i++;
+
+        while (faceobjects[j].prob < p)
+            j--;
+
+        if (i <= j)
+        {
+            // swap
+            std::swap(faceobjects[i], faceobjects[j]);
+
+            i++;
+            j--;
+        }
+    }
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            if (left < j) qsort_descent_inplace(faceobjects, left, j);
+        }
+        #pragma omp section
+        {
+            if (i < right) qsort_descent_inplace(faceobjects, i, right);
+        }
+    }
+}
+
+static void qsort_descent_inplace(std::vector<Object>& objects)
+{
+    if (objects.empty())
+        return;
+
+    qsort_descent_inplace(objects, 0, objects.size() - 1);
+}
+
+static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold)
+{
+    picked.clear();
+
+    const int n = faceobjects.size();
+
+    std::vector<float> areas(n);
+    for (int i = 0; i < n; i++)
+    {
+        areas[i] = faceobjects[i].rect.area();
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        const Object& a = faceobjects[i];
+
+        int keep = 1;
+        for (int j = 0; j < (int)picked.size(); j++)
+        {
+            const Object& b = faceobjects[picked[j]];
+
+            // intersection over union
+            float inter_area = intersection_area(a, b);
+            float union_area = areas[i] + areas[picked[j]] - inter_area;
+            // float IoU = inter_area / union_area
+            if (inter_area / union_area > nms_threshold)
+                keep = 0;
+        }
+
+        if (keep)
+            picked.push_back(i);
+    }
+}
+
+
+static void generate_yolox_proposals(std::vector<GridAndStride> grid_strides, float* feat_blob, float prob_threshold, std::vector<Object>& objects)
+{
+    const int num_class = 1;
+
+    const int num_anchors = grid_strides.size();
+
+    for (int anchor_idx = 0; anchor_idx < num_anchors; anchor_idx++)
+    {
+        const int grid0 = grid_strides[anchor_idx].grid0;
+        const int grid1 = grid_strides[anchor_idx].grid1;
+        const int stride = grid_strides[anchor_idx].stride;
+
+        const int basic_pos = anchor_idx * (num_class + 4 + 1);
+
+        // yolox/models/yolo_head.py decode logic
+        float x_center = (feat_blob[basic_pos+0] + grid0) * stride;
+        float y_center = (feat_blob[basic_pos+1] + grid1) * stride;
+        float w = exp(feat_blob[basic_pos+2]) * stride;
+        float h = exp(feat_blob[basic_pos+3]) * stride;
+        float x0 = x_center - w * 0.5f;
+        float y0 = y_center - h * 0.5f;
+
+        float box_objectness = feat_blob[basic_pos+4];
+        for (int class_idx = 0; class_idx < num_class; class_idx++)
+        {
+            float box_cls_score = feat_blob[basic_pos + 5 + class_idx];
+            float box_prob = box_objectness * box_cls_score;
+            if (box_prob > prob_threshold)
+            {
+                Object obj;
+                obj.rect.x = x0;
+                obj.rect.y = y0;
+                obj.rect.width = w;
+                obj.rect.height = h;
+                obj.label = class_idx;
+                obj.prob = box_prob;
+
+                objects.push_back(obj);
+            }
+            //if (obj.label == "Car"){
+                //vehicle_count++;
+                 
+
+        } // class loop
+
+    } // point anchor loop
+}
+
+
+
+static void decode_outputs(float* prob, std::vector<Object>& objects, float scale, const int img_w, const int img_h) {
+        std::vector<Object> proposals;
+
+        std::vector<int> strides = {8, 16, 32};
+        std::vector<GridAndStride> grid_strides;
+        generate_grids_and_stride(INPUT_W, strides, grid_strides);
+        generate_yolox_proposals(grid_strides, prob,  BBOX_CONF_THRESH, proposals);
+        // std::cout << "num of boxes before nms: " << proposals.size() << std::endl;
+
+        qsort_descent_inplace(proposals);
+
+        std::vector<int> picked;
+        nms_sorted_bboxes(proposals, picked, NMS_THRESH);
+
+
+        int count = picked.size();
+
+        std::cout << "num of boxes: " << count << std::endl;
+
+        
+        objects.resize(count);
+        for (int i = 0; i < count; i++)
+        {
+            objects[i] = proposals[picked[i]];
+
+            // adjust offset to original unpadded
+            // float x0 = (objects[i].rect.x) / scale;
+            // float y0 = (objects[i].rect.y) / scale;
+            // float x1 = (objects[i].rect.x + objects[i].rect.width) / scale;
+            // float y1 = (objects[i].rect.y + objects[i].rect.height) / scale;
+            float x0 = (objects[i].rect.x);
+            float y0 = (objects[i].rect.y);
+            float x1 = (objects[i].rect.x + objects[i].rect.width);
+            float y1 = (objects[i].rect.y + objects[i].rect.height);
+
+            // clip
+            x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
+            y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
+            x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
+            y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
+
+            objects[i].rect.x = x0;
+            objects[i].rect.y = y0;
+            objects[i].rect.width = x1 - x0;
+            objects[i].rect.height = y1 - y0;
+        }
+}
+
+
+/* This is a sample bounding box parsing function for the sample YoloV5 detector model */
+static bool NvDsInferParseYolox(
+    std::vector<NvDsInferLayerInfo> const& outputLayersInfo,
+    NvDsInferNetworkInfo const& networkInfo,
+    NvDsInferParseDetectionParams const& detectionParams,
+    
+    GstPad *osd_sink_pad = NULL;
+    nvosd = gst_element_factory_make ("nvdsosd", "nv-onscreendisplay");
+    osd_sink_pad = gst_element_get_static_pad (nvosd, "sink");
+        if (!osd_sink_pad)
+            g_print ("Unable to get sink pad\n");
+        else
+            gst_pad_add_probe (osd_sink_pad, GST_PAD_PROBE_TYPE_BUFFER,
+        osd_sink_pad_buffer_probe, NULL, NULL);
+        gst_object_unref (osd_sink_pad);
+
+    std::vector<NvDsInferParseObjectInfo>& objectList)
+{
+    float* prob = (float*)outputLayersInfo[0].buffer;
+    std::vector<Object> objects;
+    int img_w = 640;
+    int img_h = 640;
+    float scale = std::min(INPUT_W / (img_w*1.0), INPUT_H / (img_h*1.0));
+    decode_outputs(prob, objects, scale, img_w, img_h);
+    
+    for(auto& r : objects) {
+
+                     
+        if (r.label == "car"){   
+	    NvDsInferParseObjectInfo oinfo;
+          
+	    oinfo.classId = r.label;
+        oinfo.left    = static_cast<unsigned int>(r.rect.x);
+	    oinfo.top     = static_cast<unsigned int>(r.rect.y);
+	    oinfo.width   = static_cast<unsigned int>(r.rect.width);
+	    oinfo.height  = static_cast<unsigned int>(r.rect.height);
+	    oinfo.detectionConfidence = r.prob;
+	    objectList.push_back(oinfo);
+            }
+            else{
+            std::cout << "no cars" << std::endl;
+    }
+    return true;
+}
+
+extern "C" bool NvDsInferParseCustomYolox(
+    std::vector<NvDsInferLayerInfo> const &outputLayersInfo,
+    NvDsInferNetworkInfo const &networkInfo,
+    NvDsInferParseDetectionParams const &detectionParams,
+    std::vector<NvDsInferParseObjectInfo> &objectList)
+{
+    return NvDsInferParseYolox(
+        outputLayersInfo, networkInfo, detectionParams, objectList);
+}
+
+/* Check that the custom function has been defined correctly */
+CHECK_CUSTOM_PARSE_FUNC_PROTOTYPE(NvDsInferParseCustomYolox);
